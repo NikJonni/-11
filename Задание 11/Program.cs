31194bc1ed9bf826004a118e93ce528a9a778f44
Никита Егоров <nik.egorov1994@mail.ru>
2021-12-11 13:00:46 +0300

    Добавьте файлы проекта.

---
+++ Задание 11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Решение уравнения : 0=kx+b");
+            Console.Write("Введите значение 'k' = ");
+            double k1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение 'b' = ");
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            Equation task11 = new Equation();
+            Console.Write("x = {0:f1}", task11.ToRoot(k1, b1));
+            Console.ReadKey();
+        }
+        struct Equation
+        {
+            public double k;
+            public double b;
+            public Equation(double k, double b)
+            {
+                this.k = k;
+                this.b = b;
+            }
+
+            public double ToRoot(double k, double b)
+            {
+                double x = (b * (-1)) / k;
+                return x;
+            }
+        }
+    }
+
+}
+
